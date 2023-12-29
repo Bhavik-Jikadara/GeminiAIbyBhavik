8a72a45454bc13feb6b3b2fc5b000ebdb02ae669
bhavikjikadara <bhavikjikadara33523@gmail.com>
2023-12-29 18:20:00 +0530

    Initially all necessary file upload on GitHub for Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Gemini AI by Bhavik
+
+* I've created an idea for text-generating static web using the Flask framework in Python. We have to enter the text, and they generate that related content by using the Gemini-Pro model created by Google DeepMind, especially text-related text in Gemini AI.
+
+[Offical-Google-DeepMind-Website]{https://deepmind.google/technologies/gemini/#introduction}
+
+► Follow:
+* Linkedin Link: https://www.linkedin.com/in/bhavikjikadara
+* Github Link: https://github.com/Bhavik-Jikadara
+* Facebook Link: https://www.facebook.com/Bhavikjikadara07
+* Instagram Link: https://www.instagram.com/bhavikjikadara/
+* twitter Link: https://twitter.com/BhavikJikadara1
+
+►  Subscribe
+* https://www.youtube.com/channel/UC7Bp_sYQmAryrrPqvUp6PwQ
+
+►  Donate & Support us
+* https://www.paypal.com/paypalme/bhavikjikadara
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import google.generativeai as genai
+
+app = Flask(__name__)
+
+# setup your API_KEY
+genai.configure(
+    # Put the key in the GOOGLE_API_KEY
+    api_key="AIzaSyDK85o34hK764Jn_d25dYl0Q0I23Cxp8jA"
+)
+
+model = genai.GenerativeModel(model_name='gemini-pro')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/response', methods=['GET', 'POST'])
+def response():
+    return render_template("response.html")
+
+
+@app.route('/generate', methods=['GET', 'POST'])
+def generate():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        response = model.generate_content(text)
+        
+    return render_template('response.html', gen_response=response.text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
